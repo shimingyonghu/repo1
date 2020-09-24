4a9e0a7285317076ff7f561e3d3e3ace31b636f2
root <1326366412@qq.com>
2020-09-24 21:42:46 +0800

    test提交

---
+++ Test/src/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * @ClassName: Test
+ * @Description: Test
+ * @author: 陈肖
+ * @Date 2020 17:11
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
